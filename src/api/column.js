d7819a23cb9ade3ce5df1abf582dd5890631d7ad
ynf <1751985863@qq.com>
2020-06-04 01:02:45 +0800

    后台0.0.2版本

---
+++ src/api/column.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+const group_name = 'article'
+const api_name = 'column'
+export default {
+  getList() {
+    return request({
+      url: `/${api_name}`,
+      method: 'get'
+    })
+  },
+  search(page, size, searchMap) {
+    return request({
+      url: `/${api_name}/search/${page}/${size}`,
+      method: 'post',
+      data: searchMap
+    })
+  },
+  save(pojo) {
+    return request({
+      url: `/${api_name}`,
+      method: 'post',
+      data: pojo
+    })
+  },
+  findById(id) {
+    return request({
+      url: `/${api_name}/${id}`,
+      method: 'get'
+    })
+  },
+  update(id, pojo) {
+    if (id === null || id === '') {
+      return this.save(pojo)
+    }
+    return request({
+      url: `/${api_name}/${id}`,
+      method: 'put',
+      data: pojo
+    })
+  },
+  deleteById(id) {
+    return request({
+      url: `/${api_name}/${id}`,
+      method: 'delete'
+    })
+  },
+  examine(id){
+    return request({
+      url: `/${api_name}/examine/${id}`,
+      method: 'put'
+    })
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import router from './router'//路由
+import store from './store'//状态管理
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+Vue.use(ElementUI, { locale })
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
